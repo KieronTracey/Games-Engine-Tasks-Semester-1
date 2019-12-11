e2d8458d80fc2af2954a75baca4b4db98171479e
KieronTracey <57526111+KieronTracey@users.noreply.github.com>
2019-12-11 13:08:05 +0000

    Add files via upload

---
+++ Chapter_4 ConvertingFahrenheitToCelsius/ConvertingFahrenheitToCelsius/ConvertingFahrenheitToCelsius.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	float fahrenheit, celsius;
+
+	fahrenheit = 95.0f;
+	celsius = (fahrenheit - 32) * 0.5556f;
+
+
+		cout << fahrenheit << "F is equal to " << celsius << "C" << endl;
+}
